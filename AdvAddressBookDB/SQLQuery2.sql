adb5d768ada837a0a47ce102fbac0e7286fd6bcf
nk321626 <nk321626@gmail.com>
2022-06-12 00:12:27 +0530

    Add: Create Address Book Table

--- AdvAddressBookDB/SQLQuery2.sql
+++ AdvAddressBookDB/SQLQuery2.sql
@@ -1,3 +1,15 @@
 ﻿-----------------Creating the DataBase AddressBook (UC1)-----------------
 create database AdvAddressBookDB;
 use AdvAddressBookDB;
+
+-----------------Creating the AddressBook Table (UC2)--------------------
+Create Table AddressBook(
+	FirstName varchar(50) not null,
+	LastName varchar(50),
+	Address varchar(255),
+	City varchar(50),
+	StateName varchar(50),
+	ZipCode int,
+	PhoneNum bigint,
+	EmailId varchar(50)
+);
